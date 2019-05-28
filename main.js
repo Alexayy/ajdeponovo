e3eda1ecaafeb6e00347a2d9882882dabbddf95b
Alexayy <aleksa.cakic@gmail.com>
2019-05-28 14:20:38 +0200

    magic

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
+function allowDrop(ev) {
+    ev.preventDefault();
+}
 
-        function allowDrop(ev) {
-            ev.preventDefault();
-        }
+// Zove funkciju onDragStart i specifira sta ce se ragovati
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id); // postavlja tip podatka i vrednost
+    // postoo cita "text", setovace cu text u item1.1"
+}
 
-        // Zove funkciju onDragStart i specifira sta ce se ragovati
-        function drag(ev) {
-            ev.dataTransfer.setData("text", ev.target.id); // postavlja tip podatka i vrednost
-            // postoo cita "text", setovace cu text u item1.1"
-        }
-
-        // kada se dropuje, drop event se aktivira
-        // zove se funkcija drop(ev)
-        function drop(ev) {
-            ev.preventDefault();
-            var data = ev.dataTransfer.getData("text");
-            ev.target.appendChild(document.getElementById(data));
-        }
+// kada se dropuje, drop event se aktivira
+// zove se funkcija drop(ev)
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
